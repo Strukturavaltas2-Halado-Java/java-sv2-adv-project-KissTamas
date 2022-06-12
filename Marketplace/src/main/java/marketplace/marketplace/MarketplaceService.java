4e55b53a4f0e42baf3a57692fac23d58313bce81
k-tam-8 <fluff8@gmail.com>
2022-06-12 10:28:01 +0200

    Add service

--- Marketplace/src/main/java/marketplace/marketplace/MarketplaceService.java
+++ Marketplace/src/main/java/marketplace/marketplace/MarketplaceService.java
@@ -1,4 +1,16 @@
 package marketplace.marketplace;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class MarketplaceService {
+
+    private UserRepository userRepository;
+
+    private AdRepository adRepository;
+
+    
+
 }

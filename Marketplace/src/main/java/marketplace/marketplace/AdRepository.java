b5983f3ecca6d93a2533df535da6ebe1b7cd34ba
k-tam-8 <fluff8@gmail.com>
2022-06-12 10:24:26 +0200

    Add repositories

---
+++ Marketplace/src/main/java/marketplace/marketplace/AdRepository.java
@@ -0,0 +1,8 @@
+package marketplace.marketplace;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdRepository extends JpaRepository<Ad, Long> {
+}
